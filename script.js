b94f982cf4f8e5dd16923cd8833a6b021f25d3a8
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-06-22 16:50:27 +0200

    work in progress: refactoring js to object oriented

--- script.js
+++ script.js
@@ -28,82 +28,133 @@ const selectOperation = (operation) => {
   }
 }
 
-/* Display */
-const updateDisplay = (text) => {
-  document.querySelector('#display').innerHTML = text;
-};
-
-let currentNumber = '0';
-const updateCurrentNumber = (digit) => {
-  if (currentNumber === '0') {
-    currentNumber = '';
+/* Class Keyboard */
+class Keyboard {
+  constructor(identifier) {
+    this._element = document.querySelector(identifier);
+    this._digits = this._element.querySelectorAll('.digit');
+    this._operations = this._element.querySelectorAll('.operation');
+    this._allClear = this._element.querySelector('#all-clear');
+    this._invert = this._element.querySelector('#invert');
+    this._percent = this._element.querySelector('#percent');
   }
-  if (digit === '.' && currentNumber.includes('.')) {
-    return;
+
+  get digits() {
+    return this._digits;
   }
-  currentNumber += digit;
-  updateDisplay(currentNumber);
-};
 
-const resetCurrentNumber = () => {
-  currentNumber = '0';
-  updateDisplay(currentNumber);
-};
+  get operations() {
+    return this._operations;
+  }
 
-/* Digits */
-const handleDigit = (event) => {
-  updateCurrentNumber(event.target.dataset.digit);
-};
+  get allClear() {
+    return this._allClear;
+  }
 
-document.querySelectorAll(".digit").forEach((element) => {
-  element.addEventListener('click', handleDigit);
-});
+  get invert() {
+    return this._invert;
+  }
 
-/* Operations */
-let leftSideNumber;
-let operation;
-
-const calculateResult = () => {
-  const rightSideNumber = Number(currentNumber);
-  return operation(leftSideNumber, rightSideNumber);
+  get percent() {
+    return this._percent;
+  }
 }
 
-const handleOperation = (event) => {
-  console.log(event.target);
-  const symbol = event.target.dataset.operation;
-  if(symbol === '=') {
-    let result = calculateResult();
-    currentNumber = `${result}`;
-    updateDisplay(currentNumber);
-    leftSideNumber = null;
-    rightSideNumber = null;
-    operation = null;
-    return;
+/* Class Display */
+class Display {
+  constructor(identifier) {
+    this._element = document.querySelector(identifier);
   }
-  operation = selectOperation(symbol);
-  leftSideNumber = Number(currentNumber);
-  resetCurrentNumber();
-};
 
-document.querySelectorAll(".operation").forEach((element) => {
-  element.addEventListener('click', handleOperation);
-});
+  update(text) {
+    this._element.innerHTML = text;
+  }
+}
 
-/* All-Clear */
-document.querySelector("#all-clear").addEventListener('click', (event) => {
-  resetCurrentNumber();
-});
+/* Class Calculator */
+class Calculator {
+  constructor(keyboard, display) {
+    this._currentNumber = '0';
+    this._leftSideNumber = null;
+    this._rightSideNumber = null;
+    this._operation = null;
 
-/* Invert */
-document.querySelector("#invert").addEventListener('click', (event) => {
-  const number = Number(currentNumber) * -1;
-  currentNumber = `${number}`;
-  updateDisplay(currentNumber);
-});
+    this._keyboard = keyboard;
+    this._keyboard.digits.forEach((element) => {
+      element.addEventListener('click', (event) => {
+        this.updateCurrentNumber(event.target.dataset.digit);
+      });
+    });
 
-/* Percent */
-document.querySelector("#percent").addEventListener('click', (event) => {
-  const number = Number(currentNumber) / 100;
-  currentNumber = `${number}`;
-  updateDisplay(currentNumber);
-});
+    this._keyboard.operations.forEach((element) => {
+      element.addEventListener('click', (event) => {
+        this.handleOperation(event.target.dataset.operation);
+      });
+    });
+
+    this._keyboard.allClear.addEventListener('click', (event) => {
+      this.resetCurrentNumber();
+      this.reset();
+    });
+
+    this._keyboard.invert.addEventListener('click', (event) => {
+      const number = Number(this._currentNumber) * -1;
+      this._currentNumber = `${number}`;
+      this._display.update(this._currentNumber);
+    });
+
+    this._keyboard.percent.addEventListener('click', (event) => {
+      const number = Number(this._currentNumber) / 100;
+      this._currentNumber = `${number}`;
+      this._display.update(this._currentNumber);
+    });
+
+    this._display = display;
+  }
+
+  handleOperation(symbol) {
+    if(symbol === '=') {
+      this._rightSideNumber = Number(this._currentNumber);
+      let result = this.calculateResult();
+      this._currentNumber = `${result}`;
+      this._display.update(this._currentNumber);
+      this.reset();
+      return;
+    }
+    this._operation = selectOperation(symbol);
+    this._leftSideNumber = Number(this._currentNumber);
+    this.resetCurrentNumber();
+  }
+
+  updateCurrentNumber(digit) {
+    if (this._currentNumber === '0') {
+      this._currentNumber = '';
+    }
+
+    if (digit === '.' && this._currentNumber.includes('.')) {
+      return;
+    }
+
+    this._currentNumber += digit;
+    this._display.update(this._currentNumber);
+  }
+
+  resetCurrentNumber() {
+    this._currentNumber = '0';
+    this._display.update(this._currentNumber);
+  }
+
+  calculateResult() {
+    return this._operation(this._leftSideNumber, this._rightSideNumber)
+  }
+
+  reset() {
+    this._leftSideNumber = null;
+    this._rightSideNumber = null;
+    this._operation = null;
+  }
+}
+
+const keyboard = new Keyboard('#keyboard');
+const display = new Display('#display');
+const calculator = new Calculator(keyboard, display);
